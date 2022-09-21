da2df468e8c56733e6a67f42eec433f0851bd85b
eyupGuc <meydanguresi@gmail.com>
2022-09-21 11:16:11 +0300

    added p element

--- app.js
+++ app.js
@@ -3,3 +3,31 @@
 const addBtn = document.querySelector("#todo-button");
 const todoInput = document.querySelector("#todo-input");
 const todoUl = document.getElementById("todo-ul");
+
+addBtn.addEventListener("click", () => {
+  if (todoInput.value.trim() === "") {
+    alert("Please enter todo");
+  } else {
+    const newTodo = {
+      id: new Date().getTime(),
+      completed: false,
+      text: todoInput.value,
+    };
+    createListElement(newTodo);
+  }
+});
+
+const createListElement = (newTodo) => {
+  const li = document.createElement("li");
+  // li.id=newTodo.id;
+  li.setAttribute("id", newTodo.id);
+  const okIcon = document.createElement("i");
+  okIcon.setAttribute("class", "fas fa-check");
+  li.appendChild(okIcon);
+  console.log(li);
+  todoUl.appendChild(li);
+  const p = document.createElement("p");
+  const pTextNode = document.createTextNode(newTodo.text);
+  p.appendChild(pTextNode);
+  li.appendChild(p);
+};
